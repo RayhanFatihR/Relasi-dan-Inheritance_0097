55a921edb434874367ace7a616066465eaac4300
RayhanFatihR <rayhanfatihr76@gmail.com>
2025-05-27 14:55:34 +0700

    Tahap 2

--- Agregasi/Ibu.h
+++ Agregasi/Ibu.h
@@ -21,5 +21,11 @@ class ibu {
 void ibu::tambahAnak(anak* pAnak) {
     daftar_anak.push_back(pAnak);
 }
-void 
+void ibu::cetakAnak() {
+    cout << "Daftar anak dari ibu \"" << this->nama << "\":\n";
+    for (int i = 0; i < daftar_anak.size(); i++) {
+        cout << daftar_anak[i]->nama << endl;
+    }
+    cout << endl;
+}
 #endif
