fc4e23928b05daead54c3118e13797ba356cdc48
RayhanFatihR <rayhanfatihr76@gmail.com>
2025-05-27 16:24:50 +0700

    Tahap 18

--- Asosiasi/asosiasi.cpp
+++ Asosiasi/asosiasi.cpp
@@ -3,79 +3,79 @@
 using namespace std;
 
 class dokter;
-class pasien{
-    public:
-        string nama;
-        vector<dokter*> daftar_dokter;
-        pasien(string pNama) : nama(pNama) {
-            cout << "Pasien \"" << nama << "\" ada\n";
-        }
-        ~pasien(){
-            cout << "Pasien \"" << nama << "\" tidak ada\n";
-        }
+class pasien { // Asosiasi
+public:
+    string nama;
+    vector<dokter*> daftar_dokter;
+    pasien(string pNama) :nama(pNama) {
+        cout << "Pasien \"" << nama << "\" ada\n";
+    }
+    ~pasien() {
+        cout << "Pasien \"" << nama << "\" tidak ada\n";
+    }
         void tambahDokter(dokter*);
         void cetakDokter();
 };
 
-class dokter{
+class dokter {
     public:
         string nama;
         vector<pasien*> daftar_pasien;
-        dokter(string pNama) : nama(pNama) {
+
+        dokter(string pNama) :nama(pNama) {
             cout << "Dokter \"" << nama << "\" ada\n";
         }
-        ~dokter(){
+        ~dokter() {
             cout << "Dokter \"" << nama << "\" tidak ada\n";
         }
+
         void tambahPasien(pasien*);
         void cetakPasien();
-};
+    };
 
-void pasien::tambahDokter(dokter* pDokter) {
-    daftar_dokter.push_back(pDokter);
-}
-
-void pasien::cetakDokter() {
-    cout << "Daftar dokter ynag menangani pasien \"" << this->nama << "\":\n";
-    //auto digunakan dalam perulangan 
-    for (auto& a : daftar_dokter) {
-        cout << a->nama << endl;
+        void pasien::tambahDokter(dokter* pDokter) {
+            daftar_dokter.push_back(pDokter);
+        }
+        void pasien::cetakDokter() {
+            cout << "Daftar Dokter yang menangani pasien \"" << this->nama << "\":\n";
+            for (auto& a : daftar_dokter) { // Auto digunakan dalam perulangan for
+                cout << a->nama << "\n";
+            }
+                cout << endl;
+        }
+    void dokter::tambahPasien(pasien* pPasien) {
+        daftar_pasien.push_back(pPasien);
+        pPasien->tambahDokter(this);
     }
-    cout << endl;
-}
 
-void dokter::cetakPasien(pasien* pPasien) {
-    daftar_pasien.push_back(pPasien);
-    pPasien->tambahDokter(this);
-}
-
-void dokter::cetakPasien(){
-    cout << "Daftar pasien dari dokter \"" << this->nama << "\":\n";
-    for (auto& a : daftar_pasien) {
-        cout << a->nama << "\n";
+    void dokter::cetakPasien() {
+        cout << "Daftar Pasien dari dokter \"" << this->nama << "\":\n";
+        for (auto& a : daftar_pasien) {
+            cout << a->nama << "\n";
+        }
+        cout << endl;
     }
-    cout << endl;
-}
 
-int main(){
-    dokter* varDokter1 = new dokter("dr. Jundi");
-    dokter* varDokter2 = new dokter("dr. Alfaruq");
-    pasien* varPasien1 = new pasien("Daffa");
-    pasien* varPasien2 = new pasien("Bima");
+    int main(){
+        dokter* varDokter1 = new dokter("dr.Budi");
+        dokter* varDokter2 = new dokter("dr.Tono");
+        pasien* varPasien1 = new pasien("Andi");
+        pasien* varPasien2 = new pasien("Lia");
 
-    varDokter1->tambahPasien(varPasien1);
-    varDokter1->tambahPasien(varPasien2);
-    varDokter2->tambahPasien(varPasien1);
+        varDokter1->tambahPasien(varPasien1);
+        varDokter1->tambahPasien(varPasien2);
+        varDokter2->tambahPasien(varPasien1);
 
-    varDokter1->cetakPasien();
-    varDokter2->cetakPasien();
-    varPasien1->cetakDokter();
-    varPasien2->cetakDokter();
+        varDokter1->cetakPasien();
+        varDokter2->cetakPasien();
+        varPasien1->cetakDokter();
+        varPasien2->cetakDokter();
 
-    delete varDokter1;
-    delete varDokter2;
-    delete varPasien1;
-    delete varPasien2;
+        delete varPasien1;
+        delete varPasien2;
+        delete varDokter1;
+        delete varDokter2;
 
     return 0;
-}
+
+    }
