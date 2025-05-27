9dd3ccdd102c6d3b58985e2b19d9066c38b70d75
RayhanFatihR <rayhanfatihr76@gmail.com>
2025-05-27 15:15:02 +0700

    Tahap 4

---
+++ Agregasi/agregasi.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+#include "Ibu.h"
+#include "Anak.h"
+
+int main(){
+    ibu* varIbu = new ibu("Jundi");
+    ibu* varIbu2 = new ibu("Alfaruq");
+    anak* varAnak1 = new anak("daffa");
+}
