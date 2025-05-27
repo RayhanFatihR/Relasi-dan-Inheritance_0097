e4086e9a4b661c256f55a53318404ba777479ef9
RayhanFatihR <rayhanfatihr76@gmail.com>
2025-05-27 16:20:28 +0700

    Tahap 18

--- Inheritance/Inheritance.cpp
+++ Inheritance/Inheritance.cpp
@@ -35,3 +35,10 @@ class pelajar : public orang {
         }
 };
 
+int main(){
+    pelajar siswa1("andi laksono", "SMAN 1 Jakarta");
+    cout << siswa1.perkenalan();
+    cout << "Hasil =" << siswa1.jumlah(10, 90) << endl;
+
+    return 0;
+}
