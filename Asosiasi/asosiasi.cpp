1c3e792f45890d3a7a729f24f7a57db84abafc22
RayhanFatihR <rayhanfatihr76@gmail.com>
2025-05-27 15:38:08 +0700

    Tahap 13

--- Asosiasi/asosiasi.cpp
+++ Asosiasi/asosiasi.cpp
@@ -15,4 +15,17 @@ class pasien{
         }
         void tambahDokter(dokter*);
         void cetakDokter();
+};
+
+class dokter{
+    public:
+        string nama;
+        vector<pasien*> daftar_pasien;
+        dokter(string pNama) : nama(pNama) {
+            cout << "Dokter \"" << nama << "\" ada\n";
+        }
+        ~dokter(){
+            cout << "Dokter \"" << nama << "\" tidak ada\n";
+        }
+
 }
