3b66cc2fce41db5aa48775e8e15c65a8cec547af
RayhanFatihR <rayhanfatihr76@gmail.com>
2025-05-27 15:15:44 +0700

    Tahap 5

--- Agregasi/agregasi.cpp
+++ Agregasi/agregasi.cpp
@@ -9,4 +9,8 @@ int main(){
     ibu* varIbu = new ibu("Jundi");
     ibu* varIbu2 = new ibu("Alfaruq");
     anak* varAnak1 = new anak("daffa");
+    anak* varAnak2 = new anak("bima");
+    anak* varAnak3 = new anak("nona ambon");
+
+    
 }
